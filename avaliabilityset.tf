865c4c04bcca3f2318848943613e359100b08322
NasibaNarbutaeva <nasiba_87@hotmail.com>
2022-02-03 21:17:13 +0000

    some changes

--- avaliabilityset.tf
+++ avaliabilityset.tf
@@ -1,7 +1,7 @@
 resource "azurerm_availability_set" "tier1-AvailabilitySet" {
   name                         = "WebAvailSet"
-  location                     = azurerm_resource_group.ResourceGrps.location
-  resource_group_name          = azurerm_resource_group.ResourceGrps.name
+  location                     = azurerm_resource_group.project1.location
+  resource_group_name          = azurerm_resource_group.project1.name
 
   tags {
     environment = "tierone"
