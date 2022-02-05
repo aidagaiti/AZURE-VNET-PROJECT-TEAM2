57ea0b9dec31a0a2ca8190f90d43ea3ccc04bdb3
abdullah07 <abdullaherol07@gmail.com>
2022-02-05 12:35:26 -0500

    deleted tags

--- avaliabilityset.tf
+++ avaliabilityset.tf
@@ -3,8 +3,5 @@ resource "azurerm_availability_set" "Availabilityset" {
   location                     = azurerm_resource_group.project1.location
   resource_group_name          = azurerm_resource_group.project1.name
 
-  tags {
-    environment = "tierone"
-  }
 }
 
